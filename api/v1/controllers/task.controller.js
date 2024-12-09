10d56c6834f41b9788ec8006825a88dd433870d4
chison2707 <chiphansonzz17@gmail.com>
2024-12-09 15:00:47 +0700

    sắp xếp theo tiêu chí

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -9,7 +9,13 @@ module.exports.index = async (req, res) => {
         find.status = req.query.status;
     };
 
-    const tasks = await Task.find(find);
+    const sort = {};
+
+    if (req.query.sortKey && req.query.sortValue) {
+        sort[req.query.sortKey] = req.query.sortValue;
+    }
+
+    const tasks = await Task.find(find).sort(sort);
 
     res.json(tasks)
 };
