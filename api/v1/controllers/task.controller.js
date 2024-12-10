5f0240705fc47f3ff5472e953a63d3f18bddd442
chison2707 <chiphansonzz17@gmail.com>
2024-12-10 09:48:18 +0700

    thêm mới công việc

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -114,3 +114,21 @@ module.exports.changeMulti = async (req, res) => {
         });
     }
 };
+
+// [PATCH]/api/v1/tasks/create
+module.exports.create = async (req, res) => {
+    try {
+        const task = new Task(req.body);
+        const data = await task.save();
+        res.json({
+            code: 200,
+            message: "Update status thành công",
+            data: data
+        });
+    } catch (error) {
+        res.json({
+            code: 404,
+            message: "Không tồn tại!"
+        });
+    }
+};
